57e7d8daf5693baf77edb115469af1753ef2a312
Kriperus <krasnyhaleksandr123@gmail.com>
2026-06-25 01:17:06 +0500

    Add files via upload

---
+++ Lab_4 Task 1/Lab_4 Task 1/Program.fs
@@ -0,0 +1,80 @@
+﻿open System
+
+type 'T Btree =
+    | Node of 'T * 'T Btree * 'T Btree
+    | Nil
+
+[<EntryPoint>]
+let main args =
+    let infix root left right =
+        left ()
+        root ()
+        right ()
+
+    let iterh trav f t =
+        let rec tr t h =
+            match t with
+            | Node (x, L, R) ->
+                trav
+                    (fun () -> f x h)
+                    (fun () -> tr L (h + 1))
+                    (fun () -> tr R (h + 1))
+            | Nil -> ()
+        tr t 0
+
+    let spaces n =
+        List.fold (fun s _ -> s + "    ") "" [ 0..n ]
+
+    let printTree t =
+        printfn "\nСтруктура дерева:"
+        printfn "Уровень | Значение"
+        printfn "--------+----------"
+        iterh infix (fun x h -> printfn "   %d    | %s%O" h (spaces h) x) t
+
+    // Вставка элемента в бинарное дерево поиска
+    let rec insert t x =
+        match t with
+        | Nil -> Node(x, Nil, Nil)
+        | Node(z, L, R) ->
+            if x < z then
+                Node(z, insert L x, R)
+            else
+                Node(z, L, insert R x)
+
+    // Функция map для дерева
+    let rec treeMap f tree =
+        match tree with
+        | Nil -> Nil
+        | Node(x, left, right) ->
+            Node(f x, treeMap f left, treeMap f right)
+
+    // Генерация случайного списка целых чисел
+    let generateRandomList count =
+        let r = Random()
+        [ for _ in 1..count do
+              yield r.Next(-150, 160) ]
+
+    printfn "Введите количество элементов дерева:"
+    let count = Console.ReadLine() |> Convert.ToInt32
+
+    let randomValues = generateRandomList count
+    printfn "\nИсходный список целых чисел: %A" randomValues
+
+    // Построение исходного дерева
+    let integerTree = randomValues |> List.fold insert Nil
+    printfn "\nИсходное дерево целых чисел:"
+    printTree integerTree
+
+    printfn "\nВведите делитель для преобразования в вещественные числа:"
+    let divisor = Console.ReadLine() |> Convert.ToDouble
+
+    if divisor = 0.0 then
+        printfn "\nОшибка: деление на ноль невозможно!"
+    else
+        let floatTree =
+            treeMap (fun x -> Math.Round(float x / divisor, 3)) integerTree
+
+        printfn $"\nДерево вещественных значений (деление на {divisor}):"
+        printTree floatTree
+
+    0
---
+++ Lab_4 Task 2/Lab_4 Task 2/Program.fs
@@ -0,0 +1,80 @@
+﻿open System
+
+type Btree<'T> =
+    | Node of 'T * Btree<'T> * Btree<'T>
+    | Nil
+
+let infix root left right =
+    left ()
+    root ()
+    right ()
+
+let iterh trav f t =
+    let rec tr t h =
+        match t with
+        | Node (x, L, R) ->
+            trav
+                (fun () -> f x h)
+                (fun () -> tr L (h + 1))
+                (fun () -> tr R (h + 1))
+        | Nil -> ()
+    tr t 0
+
+let spaces n =
+    List.fold (fun s _ -> s + "    ") "" [ 0..n ]
+
+let printTree t =
+    printfn "\nСтруктура дерева:"
+    printfn "Уровень | Значение"
+    printfn "--------+----------"
+    iterh infix (fun x h -> printfn "   %d    | %s%O" h (spaces h) x) t
+
+let rec insert t x =
+    match t with
+    | Nil -> Node(x, Nil, Nil)
+    | Node(z, L, R) ->
+        if x < z then
+            Node(z, insert L x, R)
+        else
+            Node(z, L, insert R x)
+
+let rec treeFold f acc tree =
+    match tree with
+    | Nil -> acc
+    | Node(x, left, right) ->
+        let leftAcc = treeFold f acc left
+        let currentAcc = f leftAcc x
+        treeFold f currentAcc right
+
+let generateRandomStrings count =
+    let random = Random()
+    let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    [
+        for _ in 1..count do
+            let length = random.Next(3, 11)
+            let randomString =
+                [| for _ in 1..length do
+                       yield chars.[random.Next(chars.Length)] |]
+                |> System.String
+            yield randomString
+    ]
+
+[<EntryPoint>]
+let main args =
+    printfn "Введите количество элементов дерева:"
+    let count = Console.ReadLine() |> Convert.ToInt32
+
+    let randomStrings = generateRandomStrings count
+    printfn "\nИсходный список случайных строк: %A" randomStrings
+
+    // Построение исходного дерева
+    let stringTree = randomStrings |> List.fold insert Nil
+    printfn "\nИсходное дерево строк:"
+    printTree stringTree
+
+    // Вычисление суммарной длины строк с помощью fold
+    let totalLength = 
+        treeFold (fun acc str -> acc + String.length str) 0 stringTree
+    printfn "\nСуммарная длина всех строк в дереве: %d" totalLength
+
+    0
